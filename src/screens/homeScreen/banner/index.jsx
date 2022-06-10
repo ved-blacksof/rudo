fc2fc553d176e614cecc512770d48c7ede432d75
rakesh-blacksof <rakesh.blacksof@gmail.com>
2022-06-10 21:14:00 +0530

    formFixes

--- src/screens/homeScreen/banner/index.jsx
+++ src/screens/homeScreen/banner/index.jsx
@@ -3,59 +3,64 @@ import { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import Banner2 from "../banner2";
 // import second from '../../../assets/icons/'
-import "./style.css"
+import "./style.css";
 
 const Banner = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
   const [popup, setPopup] = useState(false);
   const [status, setStatus] = useState(0);
   const [tabs, setTabs] = useState(0);
   const [showHowItWork, setShowHowItWork] = useState(false);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-  };
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  // };
 
   const PopupFun = ({ title, subtitle }) => {
     return (
       <div className="banner__popup z-[1000000]">
         <div className="fixed  z-10 inset-0 overflow-y-auto bg-white">
           <div className="flex items-end  sm:items-center justify-center min-h-full p-4 text-center sm:p-0 ">
-
             <div className="relative bg-red rounded-lg text-left overflow-hidden shadow-2xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full ">
               <div className=" px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
                   <div className="mt-3 text-center sm:mt-0 sm:ml-2 sm:text-left">
-                    <h3 className="text-xl leading-6 font-bold font-epilogue text-white" id="modal-title">{title}</h3>
+                    <h3
+                      className="text-xl leading-6 font-bold font-epilogue text-white"
+                      id="modal-title"
+                    >
+                      {title}
+                    </h3>
                     <div className="mt-2">
-                      <p className="text-sm font-epilogue text-white">{subtitle}</p>
+                      <p className="text-sm font-epilogue text-white">
+                        {subtitle}
+                      </p>
                     </div>
                   </div>
                 </div>
               </div>
               <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse ">
-                <button type="button" onClick={() => setPopup(false)}
-                  className="w-full inline-flex justify-center rounded-md border-2  px-4 py-2  text-base font-medium text-white  hover:bg-white hover:text-black hover:border-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm ">
-                  OK</button>
+                <button
+                  type="button"
+                  onClick={() => setPopup(false)}
+                  className="w-full inline-flex justify-center rounded-md border-2  px-4 py-2  text-base font-medium text-white  hover:bg-white hover:text-black hover:border-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm "
+                >
+                  OK
+                </button>
               </div>
             </div>
           </div>
         </div>
-      </div >
-    )
-  }
+      </div>
+    );
+  };
 
-
-  useEffect(() => {
-
-  }, [tabs])
-
+  useEffect(() => {}, [tabs]);
 
   return (
     <section className=" bg-cream">
-      <div className="px-3 md:w-11/12 mx-auto flex  justify-around gap-y-10 gap-x-4 lg:flex-row flex-col   py-16 bg-cream">
+      <div className="px-3 md:w-11/12 mx-auto flex  justify-around gap-y-10 gap-x-6 lg:flex-row flex-col py-16 bg-cream">
         <div className=" grow-[3] shrink flex flex-col gap-4  justify-center max-w-screen-lg  lg:px-3 ">
           <h1 className=" text-3xl md:text-4xl xl:text-5xl font-epilogue font-bold md:leading-relaxed xl:leading-snug">
             Got Spare Change? <br />
@@ -68,32 +73,37 @@ const Banner = () => {
             rewarding way.
           </div>
 
-          <div className="bg-orange bg-opacity-10 py-4 px-6 font-epilogue ">
-
+          <div className="bg-orange bg-opacity-10 font-epilogue">
             {/* <div className="tabs-wrapper">
               <h4 className={tabs === 0 ? "tab active " : "tab"} onClick={() => setTabs(0)}>Signup</h4>
               <h4 className={tabs === 1 ? "tab active " : "tab"} onClick={() => setTabs(1)}>Milestones</h4>
               <h4 className={tabs === 2 ? "tab active " : "tab"} onClick={() => setTabs(2)}>Referral Counts</h4>
             </div> */}
 
-            <div className="viral-loops-wrapper">
-              <div className={tabs === 0 ? "embed-form height-100" : "embed-form height-0 "}>
-                <div data-vl-widget="embedForm"></div>
+            <div className="viral-loops-wrapper ">
+              <div
+                className={
+                  tabs === 0 ? "embed-form height-100" : "embed-form height-0 "
+                }
+              >
+                <div data-vl-widget="embedForm d "></div>
               </div>
-              <div className={tabs === 1 ? " height-100" : "embed-form height-0 "}>
+              <div
+                className={tabs === 1 ? " height-100" : "embed-form height-0 "}
+              >
                 <div data-vl-widget="milestoneWidget"></div>
               </div>
-              <div className={tabs === 2 ? " height-100" : "embed-form height-0 "}>
+              <div
+                className={tabs === 2 ? " height-100" : "embed-form height-0 "}
+              >
                 <div data-vl-widget="referralCountWidget"></div>
               </div>
-
             </div>
-
           </div>
         </div>
 
         <div className="flex items-center">
-          <div className="grow-[2] place-content-center shrink hidden xl:grid basis-96 ">
+          <div className="grow-[2] place-content-center shrink hidden xl:grid basis-[410px] ">
             <ReactPlayer
               url="https://youtu.be/fE-2h3stIhw"
               muted={true}
@@ -104,7 +114,7 @@ const Banner = () => {
               width={480}
             />
           </div>
-          <div className=" grow-[2]  shrink  place-content-center grid xl:hidden shrink-1 basis-96">
+          <div className=" grow-[2]  shrink  place-content-center grid xl:hidden shrink-1 basis-[410px]">
             <ReactPlayer
               url="https://youtu.be/fE-2h3stIhw"
               muted={true}
@@ -124,28 +134,34 @@ const Banner = () => {
               className="work-butn bg-red font-bold transition-all text-white text-xs p-2 px-5 pt-3 mt-10 bg-cyan cursor-pointer">How it works?<p className="btn-back"></p>
             </div>
           </div> */}
-
-
-
         </div>
       </div>
       <div className="" data-vl-widget="milestoneWidget"></div>
-      {
-        popup ?
-          status === 200 ?
-            <PopupFun title="You have submitted Successfully" subtitle="Thankyou for joining us" /> :
-            status === 400 ?
-              <PopupFun title="You are already registered" subtitle="Thankyou" /> :
-              <PopupFun title="Oops! something went wrong" subtitle="Please try after some time" />
-          : ""
-      }
+      {popup ? (
+        status === 200 ? (
+          <PopupFun
+            title="You have submitted Successfully"
+            subtitle="Thankyou for joining us"
+          />
+        ) : status === 400 ? (
+          <PopupFun title="You are already registered" subtitle="Thankyou" />
+        ) : (
+          <PopupFun
+            title="Oops! something went wrong"
+            subtitle="Please try after some time"
+          />
+        )
+      ) : (
+        ""
+      )}
 
-      {
-        showHowItWork ?
-          <div className="how-it-works-popup origin-center bg-cream">
-            <Banner2 setShowHowItWork={setShowHowItWork} />
-          </div> : ""
-      }
+      {showHowItWork ? (
+        <div className="how-it-works-popup origin-center bg-cream">
+          <Banner2 setShowHowItWork={setShowHowItWork} />
+        </div>
+      ) : (
+        ""
+      )}
     </section>
   );
 };
